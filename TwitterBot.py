547ecfa8e51d74e615a9379e47822c5a81788f34
Anthony Federico <dephoona@gmail.com>
2016-08-29 16:02:06 -0400

    Create TwitterBot.py

---
+++ TwitterBot.py
@@ -0,0 +1,30 @@
+from random  import randint
+from time    import sleep
+from tweepy  import OAuthHandler, API
+
+infile = open('Facts.txt', 'r')
+Facts = []
+for line in infile:
+    Facts.append(line.strip('\n'))
+infile.close()
+
+Hashtags = ['#science', '#facts', '#interestingfact', '#hmmm', '#follow', '#interesting', '#factoid', '#funfact', '#wow', '#braindrop']
+
+#Credentials to access Twitter API 
+ACCESS_TOKEN    = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+ACCESS_SECRET   = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_KEY    = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+#Initiate the connection to Twitter Streaming API
+Auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+Auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+TwitterBot = API(Auth)
+
+outfile = open('Log.txt', 'w')
+i = 1
+while i < len(Facts):
+    TwitterBot.update_status(Facts[i]+' '+Hashtags[randint(0,len(Hashtags)-1)]) # Tweet a fact as well as a random hashtag
+    outfile.write(str(i)+': '+Facts[i]) # Keep a log of tweeted facts incase server shuts off
+    sleep(randint(3000, 3600)*5) # Tweet every 4-5 hours to prevent bot recognition
+    i += 1
