ca71249b8a96b0da2a2506342ae2a891a6b95aa8
Anthony Federico <dephoona@gmail.com>
2016-08-30 10:58:26 -0400

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -26,5 +26,6 @@ i = 1
 while i < len(Facts):
     TwitterBot.update_status(Facts[i]+' '+Hashtags[randint(0,len(Hashtags)-1)]) # Tweet a fact as well as a random hashtag
     outfile.write(str(i)+': '+Facts[i]) # Keep a log of tweeted facts in case server shuts off
+    outfile.flush()
     sleep(randint(3000, 3600)*5) # Tweet every 4-5 hours to prevent bot recognition
     i += 1
